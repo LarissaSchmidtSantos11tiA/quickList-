d8c38e96dbc4538971786a1148068a5db87efb08
Larissa Schmidt <Larissa.arte2@gmail.com>
2025-06-01 07:54:17 -0300

    último ajuste da função checkItem

--- main.js
+++ main.js
@@ -60,15 +60,8 @@ function removeItem(itemName) {
 
 function checkItem(itemName) {
     const item = items.find((item) => item.name === itemName)
-
-    if(item.checked === true) {
-        item.checked = false
-    } else {
-        item.checked = true
-
-    }
-
-    console.log(items)
+    
+    item.checked = !item.checked
 
     showItemsList()
 
